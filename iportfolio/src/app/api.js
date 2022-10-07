20c74871eb8ea2aec13e8c04d0f0575f86a97d38
Martin <nitromagix@gmail.com>
2022-10-06 18:29:58 -0700

    fixed API_URL

--- iportfolio/src/app/api.js
+++ iportfolio/src/app/api.js
@@ -1,6 +1,6 @@
 //
 
-const API_URL = "http://localhost:3344/profiles/62f46b785ed04fd6b99c01f1";
+const API_URL = "http://localhost:3344/profiles/62f0547767da340acb25b9b2";
 // const API_URL = "https://secure-lowlands-12131.herokuapp.com/profiles/62f46b785ed04fd6b99c01f1/";
 
 export default API_URL;
